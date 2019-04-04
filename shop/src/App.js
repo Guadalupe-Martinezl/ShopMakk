e24fac69dcb437077f9d63e242d492796f81a23a
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-04-04 16:12:41 -0600

    media Queries

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,6 +5,61 @@ import iphone from './images/iphone.gif';
 import watch from './images/watch.jpg';
 import makeup from './images/makeup.jpg'
 import './App.css';
+import styled,{css} from 'styled-components'
+
+
+const Busqueda = styled.input.attrs(({ size }) => ({
+  margin: size || "5em",
+  padding: size || "5px"
+}))`
+  color:palevioletred ;
+  font-size: 20px;
+  border: 1px solid black;
+  border-radius: 2px;
+  color: black;
+  width: 20%;
+  max-width: 500px;
+  box-sizing: border-box;
+  transform: translate(100%, -100%);
+
+`;
+const Buscar = styled.button`
+background:  #000 ;
+color: #fff;
+font:100% "sans-serif";
+border-radius: 1em;
+border: 1px solid black;
+padding: 0.25em 1em;
+transform: translate(290%, -100%);
+width: 7%;
+max-width: 500px;
+box-sizing: border-box;
+`;
+const Sesion = styled.button`
+background:  #000 ;
+color: #fff;
+font:100% "sans-serif";
+border-radius: 1em;
+border: 1px solid black;
+padding: 0.25em 1em;
+transform: translate(250%, -100%);
+width: 12%;
+max-width: 500px;
+box-sizing: border-box;
+`;
+
+const Login = styled.button`
+background:  #000 ;
+color: #fff;
+font:100% "sans-serif";
+border-radius: 1em;
+border: 1px solid black;
+padding: 0.25em 1em;
+transform: translate(255%, -100%);
+width: 12%;
+max-width: 500px;
+box-sizing: border-box;
+`;
 
 class App extends Component {
   render() {
@@ -12,6 +67,27 @@ class App extends Component {
       <div className="App">
 
         <img class="logo" src={logo}/>
+        <Busqueda
+          type="text"
+          placeholder="Busqueda" />
+
+         <Buscar
+          name="busqueda"
+          type="submit">
+          Buscar
+         </Buscar>
+
+          <Sesion
+           name="busqueda"
+           type="submit">
+           Iniciar Sesion
+           </Sesion>
+
+           <Login
+            name="busqueda"
+            type="submit">
+            Crear Cuenta
+            </Login>
 
         <div className="card">
           <img class="iphone" src={iphone}/>
