53e088e03794aee70c8214baa548be9f6ca69b3e
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-04-04 14:44:56 -0600

    images 3

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import fondo from './images/fondo.jpg';
 import logo from './images/shopMak.png';
+import iphone from './images/iphone.gif';
+import watch from './images/watch.jpg';
+import makeup from './images/makeup.jpg'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+
         <img class="logo" src={logo}/>
+
+        <div className="card">
+          <img class="iphone" src={iphone}/>
+        </div>
+
+        <div className="card">
+        <img class="watch" src={watch}/>
+        </div>
+
+        <div className="card">
+        <img class="makeup" src={makeup}/>
+        </div>
+
       </div>
 
 
