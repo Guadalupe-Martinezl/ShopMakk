80b89b827ed650bef251bac9baf8e99549eefabe
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-04-05 16:14:46 -0600

    created the components index.js

---
+++ shop/src/Components/Gallery/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Gallery(props) {
+
+
+  return (
+    <div className="card">
+     <div className="box">
+       <img className="box" src={props.images[0],[1],[2]}/>
+       <span>{props.texto}</span>
+     </div>
+  </div>
+  )
+ }
