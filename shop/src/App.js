eca907fa672710c3db648c0a662f3f68d9e7b410
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-04-08 14:17:40 -0500

    test

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import  Gallery  from './Components/Gallery/';
-=======
 import Gallery from './Components/Gallery/';
->>>>>>> 54eb19bb28732ebd091b63a8ae0e1bcae09484b1
+
 import styled,{css} from 'styled-components'
 
 import fondo from './images/fondo.jpg';
@@ -71,13 +68,10 @@ class App extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App" >
-=======
 
       <div className="App">
 
->>>>>>> 54eb19bb28732ebd091b63a8ae0e1bcae09484b1
+
         <img class="logo" src={logo}/>
         <Busqueda
           type="text"
@@ -100,7 +94,7 @@ class App extends Component {
             type="submit">
             Crear Cuenta
             </Login>
-<<<<<<< HEAD
+
 
             <div className="App-Header" >
            <Gallery
@@ -109,28 +103,12 @@ class App extends Component {
              span={["Tecnologias","Comida","Maquillaje"]}
             />
 </div>
-=======
-<<<<<<< HEAD
-
-            <div className="App-Header" >
-            <Gallery
-              images={["http://lorempixel.com/400/200/technics/","http://lorempixel.com/400/200/technics/",
-              "http://lorempixel.com/400/200/technics/"]}
-              texto={"Tecnologias"}
-             />
-            </div>
-      </div>
+</div>
 
     );
+   }
   }
-=======
->>>>>>> e29a3e1b03470ab445f10715d34544ba6bd7a1bd
-      </div>
 
-    );
- }
->>>>>>> 54eb19bb28732ebd091b63a8ae0e1bcae09484b1
- }
 
 
 
--- shop/src/Components/Gallery/index.js
+++ shop/src/Components/Gallery/index.js
@@ -2,32 +2,9 @@ import React from "react";
 
 export default function Gallery(props) {
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 console.log("Gallery props--->", props);
 
-  return (
-    <div className="card">
-    {
-      props.images.map(
-        ( img )  => {
-          return(
-             <div className="box">
-               <img className="box" src={img}/>
-               <span>{props.texto}</span>
-             </div>
-          )
-      }
-      )
-    }
-    </div>
-
-  )
-}
-=======
-
->>>>>>> e29a3e1b03470ab445f10715d34544ba6bd7a1bd
   return (
     <div className="card">
     {
@@ -50,4 +27,3 @@ console.log("Gallery props--->", props);
   </div>
   )
  }
->>>>>>> 54eb19bb28732ebd091b63a8ae0e1bcae09484b1
