5ac9ee8691324d3e792efa564c0643942500bbe7
anatere <trianovalenzuela@gmail.com>
2019-04-10 12:14:30 -0500

    Creation of components in Login

---
+++ shop/src/Components/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import shop from '../../images/shop2.png';
+import reloj from '../../images/reloj.jpeg';
+//import './App.css';
+import styled, {css}from 'styled-components'
+
+export default function Login(props) {
+  return(
+    <img src={shop}
+
+    />
+  )
+
+}
