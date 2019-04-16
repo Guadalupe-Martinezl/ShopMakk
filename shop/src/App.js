62210a06ce8ea9f13ac605ded8f88fcc2fc3185c
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-04-16 13:22:48 -0500

    update

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Switch, Route } from "react-router-dom";
+
 
 import Gallery from './Components/Gallery/';
 import ProfilesU from './Components/Profile/';
