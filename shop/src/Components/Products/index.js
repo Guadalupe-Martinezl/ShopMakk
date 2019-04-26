fc6b3ef641f2c054f2b51415fd6db97b82e85931
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-04-26 10:31:27 -0500

    background

--- shop/src/Components/Products/index.js
+++ shop/src/Components/Products/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import Header from '../../Components/Header/';
 
 import shop from '../../images/shopMak.png';
-import fondo from '../../images/fondo.jpg';
 import medio from '../../images/perfil2.png';
 import icono from '../../images/iconoperson.jpg';
 import fondop from '../../images/fondop.jpg';
@@ -11,7 +10,11 @@ import fondop from '../../images/fondop.jpg';
 import styled, {css}from 'styled-components'
 
 const Contain = styled.div`
-
+padding: 10px;
+margin: 0;
+background: url(${fondop}) ;
+width: 1480px;
+height: 1150px;
   `;
 
 
@@ -20,6 +23,7 @@ const Contain = styled.div`
     return(
       <Contain>
        <Header />
+
       </Contain>
     )
 
