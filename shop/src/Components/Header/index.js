d98aa8b8086898d712294212ac010c6b67263da6
Guadalupe Martinezl <mariagpemartinez23@gmail.com>
2019-05-10 11:51:47 -0500

    Corrections to the sizes in components in profile

--- shop/src/Components/Header/index.js
+++ shop/src/Components/Header/index.js
@@ -11,7 +11,7 @@ const Contain = styled.div`
       width: 15%;
       height: 15%;
       border-radius: 5px;
-      transform: translate(-60%, 25%);
+      transform: translate(-50%, 35%);
      }
      .nav{
       position: absolute;
